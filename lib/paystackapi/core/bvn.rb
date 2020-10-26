4afb5d5952a44a849388456c064093dd94d7a46e
orednabale <50695255+orednabale@users.noreply.github.com>
2020-10-26 15:11:59 +0800

    Update bvn.rb

--- lib/paystackapi/core/bvn.rb
+++ lib/paystackapi/core/bvn.rb
@@ -1,7 +1,5 @@
 module Verification
 	def self.resolve_bvn(body)
-		puts "My secret key is: "
-		puts ENV['PAYSTACK_SECRET_KEY']
 		api = HTTParty.get("#{API::BASE_URL}" + "#{API::RESOLVE_BVN_PATH}/" + "#{body}",
 			:headers => { "Authorization" => "Bearer "+ENV["PAYSTACK_SECRET_KEY"], "content-type" => "application/json"})
 		return api
